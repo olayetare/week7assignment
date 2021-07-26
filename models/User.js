552d4dc37b8bbdadf9699154b3f5f8635c10e3aa
olaye tare <tareolaye@gmail.com>
2021-07-26 11:44:59 +0100

    week7 task

---
+++ models/User.js
@@ -0,0 +1,11 @@
+// make shift database
+const userList = [
+    {
+        id : 1,
+        email : "tareolaye@gmail.com",
+        fullName : "olaye tare love",
+        password : "classical"
+    }
+]
+module.exports = userList
+
---
+++ models/products.js
@@ -0,0 +1,20 @@
+let productList = [
+    {
+        id: 1,
+        name: "fork",
+        description: "picking food items",
+        image: "String",
+        price: 90
+
+    },
+    {
+        id: 2,
+        name: "plate",
+        description: "serving of dishes",
+        image: "String",
+        price:75
+
+    }
+];
+
+module.exports = productList;
